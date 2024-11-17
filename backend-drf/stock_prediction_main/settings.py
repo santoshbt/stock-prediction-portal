8ded15474afd2df08acb0c2dff1690ee0983a2f6
Santosh <bt.santosh01@gmail.com>
2024-11-17 18:24:35 +0000

    Front end app

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'api',
+    'corsheaders',
     'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -137,3 +139,7 @@ REST_FRAMEWORK = {
 # Media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+CORS_ALLOWED_ORIGINS = [
+   "http://localhost:5173"
+]
---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className='footer py-3 my-4'>
+    <hr className='border-bottom' />
+    <p className='text-light text-center'>&copy; 2024 - Stock Analysis and Prediction using ML by Santosh Turamari </p>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
